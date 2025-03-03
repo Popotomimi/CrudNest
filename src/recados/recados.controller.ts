d44674725834bbf3754e3740b48003fc19b7c183
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2025-03-03 12:19:05 -0300

    Update changes

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -1,23 +1,56 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { text } from 'stream/consumers';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { RecadosService } from './recados.service';
+
+// CRUD
+// Create -> POST -> Criar um recado
+// Read -> GET -> Ler todos os recados
+// Read -> GET -> Ler apenas um recado
+// Update -> PATCH / PUT -> Atualizar um recado
+// Delete -> DELETE -> Apagar um recado
+
+// PATCH é utilizado para atualizar dados de um recurso
+// PUT é utilizado para atualizar um recurso inteiro
 
 @Controller('recados')
 export class RecadosController {
-    // Encontrar todos os recados
-    @Get()
-    findAll() {
-        return 'Retorna todos os recados';
-    }
+  constructor(private readonly recadosService: RecadosService) {}
 
-    // Encontrar um recado
-    @Get(':id')
-    finOne(@Param('id') id: string) {
-        return `Retorna um recado com id ${id}`;
-    }
+  @HttpCode(HttpStatus.OK)
+  @Get()
+  findAll(@Query() pagination: any) {
+    const { limit = 10, offset = 0 } = pagination;
+    // return `Retorna todos os recados. Limit=${limit}, Offset=${offset}.`;
+    return this.recadosService.findAll();
+  }
 
-    // Criar um recado
-    @Post()
-    create(@Body() body: string) {
-        return body;
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.recadosService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() body: any) {
+    return this.recadosService.create(body);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: any) {
+    this.recadosService.update(id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    this.recadosService.remove(id);
+  }
 }
